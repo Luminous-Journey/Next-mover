0250870d1714ab06b8803845b5e78aebb40dc228
Thomas Ebrecht <t.ebrecht@outlook.com>
2023-07-29 20:21:45 -0700

    renamed some files, got rid of some bugs

--- Next mover/Next_mover.py
+++ Next mover/Next_mover.py
@@ -1,3 +1,4 @@
+from logging import PlaceHolder
 from time import sleep, ctime, time
 import PySimpleGUI as psg
 import numpy as np
@@ -9,45 +10,63 @@ import os
 
 os.system("title Next by Luminous_Journey")
 
-print(f'''       ___         ___  ___   ___   ___  ___   ___ 
+print(f'''
+       ___         ___  ___   ___   ___  ___   ___ 
   .'| |   |   .'|=|_.' |   | |   | `._|=|   |=|_.' 
 .'  |\|   | .'  |  ___ `.  | |  .'      |   |      
 |   | |   | |   |=|_.'  .` |=| `.       |   |      
 |   | |  .' |   |  ___ |   | |   |      `.  |      
 |___| |.'   |___|=|_.' |___| |___|        `.|      
 ''')
+
+def get_png_files_from_directory(directory):
+    png_files = []
+    for file in os.listdir(directory):
+        if file.lower().endswith('.png'):
+            png_files.append(os.path.join(directory, file))
+    return png_files
+
 psg.theme('Black')
-names = ["Asurascans", "Reaperscans", "Cyborg", "Suzeriantrans", "ThatGuyOverThere"]
-lst = psg.Combo(names, font=('CC Wild Words', 14),  expand_x=True, enable_events=False,  readonly=False, key='Name')
-layout = [[lst], [psg.Button('Ok')]], #psg.Button('Add', key='Add')]]
-window = psg.Window('Selection window', layout, size=(250, 100), icon=r"C:\Users\tebre\source\repos\Next mover\Next mover\Group_1.ico")
-event, values = window.read()
+lst = psg.Combo([], font=('CC Wild Words', 14), expand_x=True, enable_events=True, readonly=True, key='Name')
+layout = [
+    [psg.Text('Please select a directory')],
+    [psg.Input(key='-FOLDER-', readonly=True), psg.FolderBrowse()], 
+    [lst], 
+    [psg.Button('Open Folder'), psg.Button('Okay')]
+]
+
+window = psg.Window('Selection window', layout)
 
 while True:
+    event, values = window.read()
+
     if event == psg.WIN_CLOSED:
         window.close()
         exit("The selection window was closed")
-    if event == 'Ok':
+    elif event == "Open Folder":
+        directory = values['-FOLDER-'] + "//"
+        png_files = get_png_files_from_directory(directory)
+        names = [sub.replace(directory, '') for sub in png_files]
+        names = [sub.replace('.png', '') for sub in names]
+        window['Name'].update(values=names)
+        
+    elif event == 'Okay':
         for x in range(0, len(names)):
             if values['Name'] == names[int(x)]:
-                next = names[int(x)]
+                next = png_files[int(x)]
+
         print("Finding " + next + " next button starting on: " + str(time()) + " aka " + str(ctime()))
         break
-    elif event == "Open Folder":
-        window['Name'].update(values=names)
-        sleep(1)
         
 
 
 window.close()
-template = np.array(Image.open(r'C:\Users\tebre\source\repos\Next mover\Next mover\templates\\' + next.lower() + 'next.png').convert('L'))
+template = np.array(Image.open(next).convert('L'))
+image = np.array(pygui.screenshot().convert('L'))
+result = match_template(image, template)
+locations = np.where(result >= 0.9)
 
 while True:
-    image = np.array(pygui.screenshot().convert('L'))
-
-    result = match_template(image, template)
-
-    locations = np.where(result >= 0.9)
 
     if len(locations[0]) > 0:
         top_left = (locations[1][0], locations[0][0])
@@ -60,7 +79,7 @@ while True:
             pygui.moveTo(object_x, object_y)
             print("(x=" + str(object_x) + ", y=" + str(object_y)+ ") at " + ctime())
     
-        if next == names[2]:
+        if next == png_files[2]:
             sleep(.5)
 
         if len(locations[0]) > 0 and pygui.position() == (object_x, object_y) and keyboard.read_key() == 'right':
