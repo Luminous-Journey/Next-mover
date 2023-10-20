400d1f0948d97e93f209c29e370a62e274cdfa2a
Luminous Journey <77587691+Luminous-Journey@users.noreply.github.com>
2023-10-20 14:42:20 -0700

    Update lib.py
    Fixed keybinds and made popup disappear

--- Next mover/lib.py
+++ Next mover/lib.py
@@ -6,11 +6,23 @@ from time import sleep, time, ctime
 import PySimpleGUI as Psg
 import keyboard
 import pyautogui as pygui
+from PIL import Image
+import io
 
 selected = ''
 Paused = False
 pressed_keys = set()
-first = False
+first = True
+
+
+def adjust_transparency(image, alpha):
+    image = image.convert("RGBA")
+    new_image = Image.new("RGBA", image.size)
+    for x in range(image.width):
+        for y in range(image.height):
+            r, g, b, a = image.getpixel((x, y))
+            new_image.putpixel((x, y), (r, g, b, int(alpha * a)))
+    return new_image
 
 
 def on_key_event(e):
@@ -53,12 +65,10 @@ def window_event_handler(window, file_path, Paused):
     if event == Psg.WIN_CLOSED:
         return 'exit'
 
-
-    directory = values['-FOLDER-']
-    names = extension_remover(directory)
+    directory = values['Path']
 
     if extension_remover(directory) is not False:
-        window['Name'].update(values=names)
+        window['Name'].update(values=extension_remover(directory))
 
     if event == '-FOLDER-':
         store_path(directory, file_path)
@@ -66,49 +76,56 @@ def window_event_handler(window, file_path, Paused):
             Psg.popup("There are no Valid Files is this directory", title='No Valid files')
 
     elif event == 'shortcut':
-        # window['shortcut'].update(visible=False)
+        window['Name'].update(value=values['Name'])
+        if first:
+            first = not first
+            return "first"
         return "shortcut"
 
     elif event == 'Name':
         image_name = values['Name'] + '.png'
         selected = os.path.join(directory, image_name)
+        print(directory)
         window['Pause'].update(disabled=False)
-        first = not first
+        original_image = Image.open(selected)
+        new_width = int(original_image.width * 1.1)
+        new_height = int(original_image.height * 1.1)
+        resized_image = original_image.resize((new_width, new_height), Image.BILINEAR)
+        img_byte_array = io.BytesIO()
+        resized_image.save(img_byte_array, format='PNG')
+        img_bytes = img_byte_array.getvalue()
+        window['Image Text'].update(visible=True)
+        window['Image'].update(data=img_bytes, visible=True)
+        window['Name'].update(value=values['Name'])
         if not os.path.isfile(selected):
             print(f"Invalid file path: {selected}")
-
         else:
             print("Finding " + selected + " next button starting on: " + str(time()) + " aka " + str(ctime()))
+            if selected != '':
+                return selected
 
     if event == 'Pause':
         if Paused == False:
-            window['Pause'].update(button_color=Psg.theme_button_color())
-            window['Name'].update(value=values['Name'])
-            window['Name'].update(disabled=False)
             Paused = True
             return Paused
 
         elif Paused == True:
-            window['Pause'].update(button_color=('White', 'Black'))
-            window['Name'].update(value=values['Name'])
-            window['Name'].update(disabled=True)
             Paused = not Paused
             return Paused
 
-    if Paused == False and not first:
+    if Paused is False and not first:
         window['Pause'].update(button_color=Psg.theme_button_color())
         window['Name'].update(value=values['Name'])
         window['Name'].update(disabled=False)
 
-    elif Paused == True and not first:
-        window['Pause'].update(button_color='Blue')
+    elif Paused is True and not first:
+        window['Pause'].update(button_color=("Black", "White"))
         window['Name'].update(value=values['Name'])
         window['Name'].update(disabled=True)
 
     window['Name'].update(value=values['Name'])
 
-    if selected != '':
-        return selected
+    return selected
 
 
 def get_image_files_from_directory(path):
@@ -119,9 +136,8 @@ def get_image_files_from_directory(path):
             if file.lower().endswith(('.png', '.jpg')):
                 image_files.append(os.path.join(path, file))
     except FileNotFoundError:
-        if not true:
-            print('Error: No image files (.png or .jpg) found in this directory')
-            true = not true
+        pass
+
 
     if not image_files:
         pass
@@ -152,6 +168,7 @@ def color(text):
             faded += f"\033[38;2;255;{blue};0m{character}\033[0m"
         faded += "\n"
     return faded
+
 
 def euclidean_distance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
